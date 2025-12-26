1d947fb39eea832febcbee64c2744ef91d6bb2ed
kjvnb61-bit <kjvnb61@gmail.com>
2025-12-26 17:28:52 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages ke liye base path add karein
+  base: '/last-chain/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
